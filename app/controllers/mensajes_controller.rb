8fcab1b865890099afc6311995b2f5f4eed63ef9
Ezequiel <exe.zaid@gmail.com>
2010-10-07 21:55:12 -0300

    add controller mensajes and define routes

---
+++ app/controllers/mensajes_controller.rb
@@ -0,0 +1,11 @@
+class MensajesController < ApplicationController
+  def hola
+    #render :text => 'hola'  
+  end
+  def chau 
+  end
+  def algo
+  end
+  def prueba
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Prueba1::Application.routes.draw do
-  resources :personas
+  resources :personas, :path => "people"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +50,12 @@ Prueba1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "mensajes#hola"
+  match 'chau' => 'mensajes#chau', :as => :salir
+  match 'test' => 'mensajes#prueba', :as => :test
+  match 'algo' => 'mensajes#algo', :as => :algos
+
+
 
   # See how all your routes lay out with "rake routes"
 
